92add33e6c350aad276ddd5f5b86d1ba19b3d5d8
Mann Yivfouy <mannyivfouy@gmail.com>
2026-04-29 08:10:27 +0700

    mabybe complete structure v1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideTranslateService } from '@ngx-translate/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideTranslateService()
   ]
 };
--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../shared/navbar/navbar";
+import { Footer } from "../shared/footer/footer";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [Navbar, RouterOutlet, Footer],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
