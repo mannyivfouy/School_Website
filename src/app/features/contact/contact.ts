1d35198d6dc1f47261b381ea8784eb9f0a893d11
Mann Yivfouy <mannyivfouy@gmail.com>
2026-04-30 23:44:38 +0700

    featuer contact v1

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Language } from '../../services/language';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
+  currentLang: string = localStorage.getItem('lang') || 'en';
 
+  constructor(private langService : Language){}
+
+  changeLang(lang : string){
+    this.currentLang = lang;
+    this.langService.setLanguage(lang);
+  }
 }
