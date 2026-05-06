af3915856beac7a173d7399bc0b05675999c19c7
Mann Yivfouy <mannyivfouy@gmail.com>
2026-05-06 10:14:34 +0700

    fearure footer

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Language } from '../../services/language';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule, TranslateModule, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
+  currentLand: string = localStorage.getItem('lang') || 'en';
 
+  constructor(private langService: Language) {}
+
+  changeLang(lang: string) {
+    this.currentLand = lang;
+    this.langService.setLanguage(lang);
+  }
 }
