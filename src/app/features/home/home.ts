788ac4ea8120588f7a8a7c5d48355a189f43944f
Mann Yivfouy <mannyivfouy@gmail.com>
2026-05-12 18:44:38 +0700

    statscard UI

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ImageSlider } from './components/image-slider/image-slider';
 import { WelcomeMessage } from "./components/welcome-message/welcome-message";
+import { StatsCard } from "./components/stats-card/stats-card";
 
 @Component({
   selector: 'app-home',
-  imports: [ImageSlider, WelcomeMessage],
+  imports: [ImageSlider, WelcomeMessage, StatsCard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
