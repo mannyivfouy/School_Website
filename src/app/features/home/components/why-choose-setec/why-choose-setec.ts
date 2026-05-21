848432732646867a42b056a50a495d3080e62a0c
Mann Yivfouy <mannyivfouy@gmail.com>
2026-05-21 17:07:06 +0700

    new feature about setec in homepage

--- src/app/features/home/components/why-choose-setec/why-choose-setec.ts
+++ src/app/features/home/components/why-choose-setec/why-choose-setec.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Language } from '../../../../services/language';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-why-choose-setec',
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './why-choose-setec.html',
   styleUrl: './why-choose-setec.css',
 })
 export class WhyChooseSetec {
+  currentLang: string = localStorage.getItem('lang') || 'en';
 
+  constructor(private langService : Language){}
+
+  changeLang(lang : string){
+    this.currentLang = lang;
+    this.langService.setLanguage(lang);
+  }
 }
